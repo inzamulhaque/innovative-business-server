3f93fb00c80f0bb180517d5dd24daa43fd7baf71
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-07-08 23:38:32 +0600

    add

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 7000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server Running ALIF");
+  res.send("Server Running shoaib ahmad sakib");
 });
 
 app.listen(port, () => {
